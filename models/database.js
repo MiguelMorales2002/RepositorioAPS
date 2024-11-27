ff34d3ec10fb6c2ac35ecd60bfa095f45a018f9b
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-11-27 09:54:29 -0600

    Se agrego Models al proyecto

---
+++ models/database.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require("sequelize");
+// Conexión con MySQL
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+module.exports = sequelize;
